9111233304a0b0b226617324fc06f38b2d642389
carter <carter15102@gmail.com>
2026-04-05 22:54:54 -0400

    added drop table functionality

--- Data/CreateTablesGroup1.sql
+++ Data/CreateTablesGroup1.sql
@@ -1,3 +1,33 @@
+if (OBJECT_ID('Work_Log') is not NULL)
+    drop table Work_Log;
+
+if (OBJECT_ID('Crew_Truck') is not NULL)
+    drop table Crew_Truck;
+
+if (OBJECT_ID('Crew_Member') is not NULL)
+    drop table Crew_Member;
+
+if (OBJECT_ID('Manager') is not NULL)
+    drop table Manager;
+
+if (OBJECT_ID('Job_Inspector') is not NULL)
+    drop table Job_Inspector;
+
+if (OBJECT_ID('Crew') is not NULL)
+    drop table Crew;
+
+if (OBJECT_ID('Truck') is not NULL)
+    drop table Truck;
+
+if (OBJECT_ID('Street') is not NULL)
+    drop table Street;
+
+if (OBJECT_ID('Employee') is not NULL)
+    drop table Employee;
+
+
+
+
 use MIST353_Group1
 
 
